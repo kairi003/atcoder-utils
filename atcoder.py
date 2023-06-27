4330c1ebb6ca44253bc408b0b91aa0058db0a51b
kairi003 <kairi.satellite@gmail.com>
2023-06-28 04:16:01 +0900

    Update: to get from  print_task

--- atcoder.py
+++ atcoder.py
@@ -7,7 +7,7 @@ from urllib.parse import urljoin
 from datetime import datetime, timezone, timedelta
 import re
 from typing import Generator, Optional
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from requests import Session
 from urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
@@ -44,11 +44,11 @@ class AtCoderTask:
         self.name = name if name else url.split('/')[-1]
         self.ses = ses if ses is not None else Session()
         self.contest = contest
+        self.input_samples = []
+        self.output_samples = []
         self._get_samples()
 
     def _get_samples(self):
-        self.input_samples = []
-        self.output_samples = []
         if not self.url:
             return
         res = self.ses.get(self.url)
@@ -58,7 +58,30 @@ class AtCoderTask:
             pre = sec.select_one('pre')
             if h3 is None or pre is None:
                 continue
-            text = re.sub(r'\r\n?', '\n', pre.text.strip())
+            text = re.sub(r'\r\n?', '\n', pre.text.strip()) + '\n'
+            if '入力例' in h3.text:
+                self.input_samples.append(text)
+            elif '出力例' in h3.text:
+                self.output_samples.append(text)
+        return
+
+class AtCoderPrintTask(AtCoderTask):
+    def __init__(self, name: str, task_statement: Tag, contest: 'ContestBase'):
+        self.url = f'{contest.url}/tasks/{contest.name}_{name}'
+        self.name = name
+        self.ses = None
+        self.contest = contest
+        self.input_samples = []
+        self.output_samples = []
+        self._get_samples(task_statement)
+    
+    def _get_samples(self, task_statement: Tag):
+        for part in task_statement.select('.part'):
+            h3 = part.select_one('h3')
+            pre = part.select_one('pre')
+            if h3 is None or pre is None:
+                continue
+            text = re.sub(r'\r\n?', '\n', pre.text.strip()) + '\n'
             if '入力例' in h3.text:
                 self.input_samples.append(text)
             elif '出力例' in h3.text:
@@ -106,16 +129,14 @@ class AtCoderContest(ContestBase):
         while (d := self.start_time - datetime.now(timezone.utc)) > zero:
             print(f'\rContest begins in {d}', end='\r')
             time.sleep(self.WAIT)
-        tasks_url = f'{self.url}/tasks/'
+        tasks_url = f'{self.url}/tasks_print/'
         res = self.ses.get(tasks_url)
         soup = BeautifulSoup(res.content, 'lxml')
-        for tr in soup.select('tbody tr'):
-            a = tr.select_one("a[href*='/tasks/']")
-            if a is None:
-                continue
-            task_url = urljoin(tasks_url, a['href'])
-            task_name = a.text.strip().lower()
-            yield AtCoderTask(task_url, task_name, self.ses, self)
+        for task_statement in soup.select('#task-statement'):
+            task_content = task_statement.parent
+            task_name = task_content.select_one('.h2').text.split('-', 1)[0].strip().lower()
+            yield AtCoderPrintTask(task_name, task_statement, self)
+            #yield AtCoderTask(task_url, task_name, self.ses, self)
 
 
 class VirtualContest(ContestBase):
